787e6557484c87ae0cff1bbf7642252fe6d95e5a
Midiatra <Midiatra@DESKTOP-P6VL3T2>
2023-01-17 10:31:43 +0300

    Add project files.

---
+++ CampIntro/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace CampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            Course course1 = new Course() 
+            { 
+                CourseName = "C#",
+                Trainer = "Engin Demiroğ",
+                WatchingRate= 68,
+            };
+
+            Course course2 = new Course()
+            {
+                CourseName = "Java",
+                Trainer = "Kerem Varış",
+                WatchingRate = 64,
+            };
+
+            Course course3 = new Course()
+            {
+                CourseName = "Phyton",
+                Trainer = "Berkay Bilgin",
+                WatchingRate = 80,
+            };
+
+            //Console.WriteLine(course1.CourseName+ " : " + course1.Trainer);
+
+            Course[] courses = new Course[] 
+            { 
+                course1, course2,course3 
+            };
+
+            Console.WriteLine();
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.CourseName+": "+ c.Trainer);
+            }
+        }
+    }
+
+    class Course
+    {
+        public string CourseName { get; set; }
+        public string Trainer { get; set; }
+        public int WatchingRate { get; set; }
+    }
+
+    
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Id = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Id = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Id+"\n"+urun.Adi + "\n" + urun.Aciklama);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("------------METODLAR--------------");
+
+            //instance -> örnek
+            //Mor küçük kutu resmi metod anlamına gelir
+            //İngiliz anahtarı 
+            //encapsulation ürünün parametre(patlamaya müsait) olarak değilde özellik olarak eklenmesi örneği
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12,10);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12,9);
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12,8);
+
+
+        }
+
+
+    }
+}
